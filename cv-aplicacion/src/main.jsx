104654e77a8b51fe06eac5de70a82c5dc70e8a90
LucianoAndresLugani <63163767+LucianoAndresLugani@users.noreply.github.com>
2023-10-19 09:00:01 -0300

    Test

--- cv-aplicacion/src/main.jsx
+++ cv-aplicacion/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Person from './Person.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <Person/>
   </React.StrictMode>,
 )
